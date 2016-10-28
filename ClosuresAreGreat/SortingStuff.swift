4ea1ceaaaf13847f39d6be71fabd848474a3e8fa
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-10-28 09:11:14 -0400

    commit

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -16,9 +16,60 @@ struct ToyBin {
     var bowlingPins: [BowlingPin] = []
     var musicCDs: [MusicCD] = []
     
-    // TODO: Implement all of the sort functions (lets organize this toy bin!)
+    mutating func sortShips() {
+        let sortedShips = ships.sorted { (shipOne, shipTwo) -> Bool in
+            if shipOne.name == "Titanic" {
+                return true
+            } else if shipTwo.name == "Titanic" {
+                return false
+            }
+            if shipOne.age > shipTwo.age {
+                return true
+            }
+            return false
+        }
+        ships = sortedShips
+    }
     
- 
+    
+    mutating func sortBooks() {
+        let sortedBooks = books.sorted { $0.name < $1.name }
+        books = sortedBooks
+    }
+    
+    mutating func sortBowlingPins() {
+        let sortedBowlingPins = bowlingPins.sorted { $0.color.rawValue < $1.color.rawValue }
+        bowlingPins = sortedBowlingPins
+    }
+
+    mutating func sortMusicCDs() {
+        let sortedMusicCDs = musicCDs.sorted { (musicOne, musicTwo) -> Bool in
+            if musicOne.name == "Drake" {
+                return true
+            } else if musicTwo.name == "Drake" {
+                return false
+            }
+            
+            if musicOne.name < musicTwo.name {
+                return true
+            } else {
+                
+                return false
+            }
+        }
+        musicCDs = sortedMusicCDs
+    }
+    
+    
+    mutating func changeColorOfAllPins(to color: Color) {
+        let newPins = bowlingPins.map { (pin) -> BowlingPin in
+            var colorPin = pin
+            colorPin.changeColor(to: color)
+            return colorPin
+        }
+        
+        bowlingPins = newPins
+    }
 }
 
 
@@ -39,13 +90,16 @@ struct BowlingPin {
     var name: String
     var film: String
     var color: Color
+    
+    mutating func changeColor(to color: Color) {
+        self.color = color
+    }
 }
 
 
 enum Color: Int {
     case red, organe, yellow, green, blue, indigo, violet
 }
-
 
 
 struct MusicCD {
--- ClosuresAreGreat/ViewController.swift
+++ ClosuresAreGreat/ViewController.swift
@@ -12,10 +12,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        performOperation(with: 3, 4, 5) { (numOne, numTwo) -> Double in
+
+            return 0
+        }
+
+        
     }
     
-  
-    
-
+    func performOperation(with numbers: Double..., operatrion: (Double, Double) -> Double) -> Double {
+        var returnVar = numbers
+        numbers.reduce(numbers[0]) { (numOne, numTwo) -> Double in
+            print("numOne: \(numOne)")
+            print("numTwo: \(numTwo)")
+            // either want to ignore numOne after the first iteration or make numOne = numTwo and ignore numTwo -> returning a double so somehow return the num you want to use in next iteration? 
+            if numTwo != numbers[0] {
+                returnVar.remove(at: 0)
+            }
+            returnVar = numbers
+//            if sum < numOne {
+//                sum += numOne
+//                sum - numTwo
+//
+//            } else {
+//                sum += numTwo
+//            }
+//            
+//            print("sum \(sum)")
+//            return numOne
+//        }
+        
+//        return sum
+            print(returnVar)
+            return numbers
+        }
+        
+        return numbers
+    }
     
 }
